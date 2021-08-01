3a25c19258348171d3a6939faba91fcd9f19c893
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2021-08-01 14:01:27 -0400

    main() takeaway for easier debugging

--- die_cropper.py
+++ die_cropper.py
@@ -73,96 +73,96 @@ def get_match(window, compareCrop, x, y):
             # yield "null"
 
 
-def main(): 
-    # construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    # DELETEEEEE ap.add_argument("-i", "--image", required=True, help="Path to the image")
-    ap.add_argument("-c", "--compareCrops", required=True,
-        help="crop image to compare to location")
-    ap.add_argument("-f", "--full", required=True,
-        help="full image location")
-    args = vars(ap.parse_args())
-    
-    # load the full and comparing crop images
-    image = cv2.imread(args["full"])
-    compareCrop = cv2.imread(args["compareCrops"])
-    
-    windowSizeFraction = 0.06 # What fraction of full image the window should be
-    stepSizeFraction = 0.02 # What fraction of full image the window should step by
+# def main(): # MAKE SURE TO INDENT THE REST
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+# DELETEEEEE ap.add_argument("-i", "--image", required=True, help="Path to the image")
+ap.add_argument("-c", "--compareCrops", required=True,
+    help="crop image to compare to location")
+ap.add_argument("-f", "--full", required=True,
+    help="full image location")
+args = vars(ap.parse_args())
+
+# load the full and comparing crop images
+image = cv2.imread(args["full"])
+compareCrop = cv2.imread(args["compareCrops"])
+
+windowSizeFraction = 0.06 # What fraction of full image the window should be
+stepSizeFraction = 0.02 # What fraction of full image the window should step by
+
+# Predefine next for loop's parameters 
+layer = 0 # Layer of resolution-downscaled
+
+# loop over the image pyramid
+for (resized, resizedCrop) in pyramid(image, compareCrop, scale=1.5):
+    # Parameter set
+    stepSize=round(resized.shape[1] * stepSizeFraction)
+    winW = round(resized.shape[1] * windowSizeFraction) # Scales window width according to full image resolution
+    winH = round(resized.shape[1] * windowSizeFraction) # Scales window height according to full image resolution
+    windowSize = (winW, winH)
     
     # Predefine next for loop's parameters 
-    layer = 0 # Layer of resolution-downscaled
+    prev_y1 = 0
+    prev_x1 = 0
+    rowNum = 0
+    colNum = 0
     
-    # loop over the image pyramid
-    for (resized, resizedCrop) in pyramid(image, compareCrop, scale=1.5):
-        # Parameter set
-        stepSize=round(resized.shape[1] * stepSizeFraction)
-        winW = round(resized.shape[1] * windowSizeFraction) # Scales window width according to full image resolution
-        winH = round(resized.shape[1] * windowSizeFraction) # Scales window height according to full image resolution
-        windowSize = (winW, winH)
+    # loop over the sliding window for each layer of the pyramid
+    for (x, y, window) in sliding_window(resized, stepSize, windowSize):
+        # if the window does not meet our desired window size, ignore it
+        if window.shape[0] != winH or window.shape[1] != winW:
+            continue
         
-        # Predefine next for loop's parameters 
-        prev_y1 = 0
-        prev_x1 = 0
-        rowNum = 0
-        colNum = 0
+        # since we do not have a classifier, we'll just draw the window
+        clone = resized.copy()
+        cv2.rectangle(clone, (x, y), (x + winW, y + winH), (255, 0, 180), 20) # ORIGINAL COLOR IS 0,255,0
+        # Add rect to area already saved
+        clone = cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 20)
+        cloneResize = cv2.resize(clone, (1728, 972))
+        cv2.imshow("Window", cloneResize)
+        cv2.waitKey(1)
+        # time.sleep(0.5) # sleep time in ms after each window step
         
-        # loop over the sliding window for each layer of the pyramid
-        for (x, y, window) in sliding_window(resized, stepSize, windowSize):
-            # if the window does not meet our desired window size, ignore it
-            if window.shape[0] != winH or window.shape[1] != winW:
-                continue
-            
-            # since we do not have a classifier, we'll just draw the window
-            clone = resized.copy()
-            cv2.rectangle(clone, (x, y), (x + winW, y + winH), (255, 0, 180), 20) # ORIGINAL COLOR IS 0,255,0
-            # Add rect to area already saved
-            clone = cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 20)
-            cloneResize = cv2.resize(clone, (1728, 972))
-            cv2.imshow("Window", cloneResize)
-            cv2.waitKey(1)
-            # time.sleep(0.5) # sleep time in ms after each window step
-            
-            # Scans window for matched image
-            # ==================================================================================
-            # Scans window and grabs cropped image coordinates relative to window
-            win_x1, win_y1, win_x2, win_y2 = get_match(window, resizedCrop, x, y)
-            
-            # Saves cropped image and names with coordinates
-            if win_x1 != "null":
-                # Turns cropped image coordinates relative to window to full image coordinates
-                x1 = x + win_x1
-                y1 = y + win_y1
-                x2 = x + win_x2
-                y2 = y + win_y2
-                
-                # Makes sure same image does not get saved as different names
-                if y1 >= (prev_y1 + round(stepSize/3) ) or y1 <= (prev_y1 - round(stepSize/3) ):
-                    rowNum += 1
-                    colNum = 1
-                else:
-                    if x1 >= (prev_x1 + round(stepSize/3) ) or x1 <= (prev_x1 - round(stepSize/3) ):
-                        colNum += 1
-                
-                # NEEDS A CHECK TO SEE IF FIRST X IN PREVIOUS Y-ROW IS THE SAME
-                #   IF IT ISN'T, THEN MAKE PREVIOUS FIRST X IN PREVIOUS ROW
-                #   HAVE A COLUMN_NUMBER += 1 AND DELETE OLD SAVE AND RESAVE
-                #   WITH NEW NAME
-                
-                # Gets cropped image and saves cropped image
-                croppedImage = window[win_y1:win_y2, win_x1:win_x2]
-                cv2.imwrite("./Images/Cropped_Images/L{}-Row_{}-Col_{}.jpg".format(layer, rowNum, colNum), croppedImage)
-                
-                prev_y1 = y1
-                prev_x1 = x1
-                
-            # ==================================================================================
+        # Scans window for matched image
+        # ==================================================================================
+        # Scans window and grabs cropped image coordinates relative to window
+        win_x1, win_y1, win_x2, win_y2 = get_match(window, resizedCrop, x, y)
         
-        layer += 1
+        # Saves cropped image and names with coordinates
+        if win_x1 != "null":
+            # Turns cropped image coordinates relative to window to full image coordinates
+            x1 = x + win_x1
+            y1 = y + win_y1
+            x2 = x + win_x2
+            y2 = y + win_y2
+            
+            # Makes sure same image does not get saved as different names
+            if y1 >= (prev_y1 + round(stepSize/3) ) or y1 <= (prev_y1 - round(stepSize/3) ):
+                rowNum += 1
+                colNum = 1
+            else:
+                if x1 >= (prev_x1 + round(stepSize/3) ) or x1 <= (prev_x1 - round(stepSize/3) ):
+                    colNum += 1
+            
+            # NEEDS A CHECK TO SEE IF FIRST X IN PREVIOUS Y-ROW IS THE SAME
+            #   IF IT ISN'T, THEN MAKE PREVIOUS FIRST X IN PREVIOUS ROW
+            #   HAVE A COLUMN_NUMBER += 1 AND DELETE OLD SAVE AND RESAVE
+            #   WITH NEW NAME
+            
+            # Gets cropped image and saves cropped image
+            croppedImage = window[win_y1:win_y2, win_x1:win_x2]
+            cv2.imwrite("./Images/Cropped_Images/L{}-Row_{}-Col_{}.jpg".format(layer, rowNum, colNum), croppedImage)
+            
+            prev_y1 = y1
+            prev_x1 = x1
+            
+        # ==================================================================================
+    
+    layer += 1
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
     
     
     

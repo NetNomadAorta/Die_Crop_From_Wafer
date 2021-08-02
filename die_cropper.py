b7c7ed158c5981bc89efc5583e1af86328211774
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2021-08-01 21:05:33 -0400

    Major Changes
    - Easier parameter change section
    - Deleted parameter argument
    - Splits obvious defects from non obvious defects into two folders
    - - These will include confidence level in name
    - If die is matched twice in two different scanning windows, it will save the higher confidence match instead
    - draws orange boxes over potential defect dies

--- die_cropper.py
+++ die_cropper.py
@@ -5,11 +5,20 @@ import json
 import argparse
 import time
 import cv2
+import os
+import numpy as np
 
-# Example argument
-#   cd C:\Users\troya\.spyder-py3\Die_Crop_From_Wafer
-#   python die_cropper.py -f Images/Images_to_Scan/Original.jpg -c Images/Images_to_Compare_for_Cropping/train_2.jpg
-#   run die_cropper.py -f Images/Images_to_Scan/Original.jpg -c Images/Images_to_Compare_for_Cropping/train_2.jpg
+# User Parameters/Constants to Set
+MATCH_CL = 0.50 # Minimum confidence level (CL) required to match image to scanned image
+SPLIT_MATCHES_CL =  0.80 # Splits matches to no defects and potential defects - between MATCH_CL and 1
+FULL_IMAGE_PATH = "Images/Images_to_Scan/Original.jpg"
+GOLDEN_IMAGE_PATH = "Images/Images_to_Compare_for_Cropping/toCompare.jpg"
+SLEEP_TIME = 0.0 # Time to sleep in seconds between each window step
+
+def deleteDirContents(dir):
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+
 
 def pyramid(image, compareCrop, scale=1.5, minSize=(500, 500)):
     # yield the original image
@@ -32,18 +41,17 @@ def pyramid(image, compareCrop, scale=1.5, minSize=(500, 500)):
         # yield the next image in the pyramid
         yield (image, compareCrop)
 
-def sliding_window(image, stepSize, windowSize): # stepSize normally 4 to 8 (pixels)
+
+def slidingWindow(image, stepSizeX, stepSizeY, windowSize): # stepSize normally 4 to 8 (pixels)
     # slide a window across the image
-    for y in range(0, image.shape[0], stepSize):
-        for x in range(0, image.shape[1], stepSize):
+    for y in range(0, image.shape[0], stepSizeY):
+        for x in range(0, image.shape[1], stepSizeX):
             # yield the current window
             yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
 
 
-
 # Comparison Scan
-# ======================
-def get_match(window, compareCrop, x, y):
+def getMatch(window, compareCrop, x, y):
     h1, w1, c1 = window.shape
     h2, w2, c2 = compareCrop.shape
     
@@ -52,43 +60,31 @@ def get_match(window, compareCrop, x, y):
         res = cv2.matchTemplate(window, compareCrop, method)   
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         
-        if max_val>0.65: # ORIGINALLY: 0.98
+        if max_val > MATCH_CL: 
             print("\nFOUND MATCH")
             print("max_val = ", max_val)
             print("Coordinates: x1:", x + max_loc[0], "y1:", y + max_loc[1], \
                   "x2:", x + max_loc[0]+w2, "y2:", y + max_loc[1]+h2)
             
             # Gets coordinates of cropped image
-            return (max_loc[0], max_loc[1], max_loc[0] + w2, max_loc[1] + h2)
-            # yield max_loc[0]
-            # yield max_loc[1]
-            # yield max_loc[0]+w2
-            # yield max_loc[1]+h2 # Gets coordinates of cropped image
+            return (max_loc[0], max_loc[1], max_loc[0] + w2, max_loc[1] + h2, max_val)
         
         else:
-            return ("null", "null", "null", "null")
-            # yield "null"
-            # yield "null"
-            # yield "null"
-            # yield "null"
+            return ("null", "null", "null", "null", "null")
 
 
-# def main(): # MAKE SURE TO INDENT THE REST
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-# DELETEEEEE ap.add_argument("-i", "--image", required=True, help="Path to the image")
-ap.add_argument("-c", "--compareCrops", required=True,
-    help="crop image to compare to location")
-ap.add_argument("-f", "--full", required=True,
-    help="full image location")
-args = vars(ap.parse_args())
+# MAIN():
+# =============================================================================
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+# Deletes contents in cropped and split folders
+deleteDirContents("./Images/Cropped_Images/")
+deleteDirContents("./Images/Splitted_Cropped_Images/No_Defects/")
+deleteDirContents("./Images/Splitted_Cropped_Images/Potential_Defects/")
 
 # load the full and comparing crop images
-image = cv2.imread(args["full"])
-compareCrop = cv2.imread(args["compareCrops"])
-
-windowSizeFraction = 0.06 # What fraction of full image the window should be
-stepSizeFraction = 0.02 # What fraction of full image the window should step by
+image = cv2.imread(FULL_IMAGE_PATH)
+compareCrop = cv2.imread(GOLDEN_IMAGE_PATH)
 
 # Predefine next for loop's parameters 
 layer = 0 # Layer of resolution-downscaled
@@ -96,37 +92,48 @@ layer = 0 # Layer of resolution-downscaled
 # loop over the image pyramid
 for (resized, resizedCrop) in pyramid(image, compareCrop, scale=1.5):
     # Parameter set
-    stepSize=round(resized.shape[1] * stepSizeFraction)
-    winW = round(resized.shape[1] * windowSizeFraction) # Scales window width according to full image resolution
-    winH = round(resized.shape[1] * windowSizeFraction) # Scales window height according to full image resolution
+    winW = round(resizedCrop.shape[1] * 1.5) # Scales window width according to full image resolution
+    winH = round(resizedCrop.shape[0] * 1.5) # Scales window height according to full image resolution
     windowSize = (winW, winH)
+    stepSizeX = round(winW / 3)
+    stepSizeY = round(winH / 3)
     
     # Predefine next for loop's parameters 
     prev_y1 = 0
     prev_x1 = 0
     rowNum = 0
     colNum = 0
+    prev_matchedCL = 0
+    # TESTING BELOW
+    BadX1 = 0
+    BadY1 = 0
+    BadX2 = 0
+    BadY2 = 0
+    
+    # TESTING BELOW FOR SAVING IMAGE WITH BAD DIE BOXES
+    clone2 = image.copy()
     
     # loop over the sliding window for each layer of the pyramid
-    for (x, y, window) in sliding_window(resized, stepSize, windowSize):
+    for (x, y, window) in slidingWindow(resized, stepSizeX, stepSizeY, windowSize):
         # if the window does not meet our desired window size, ignore it
         if window.shape[0] != winH or window.shape[1] != winW:
             continue
         
-        # since we do not have a classifier, we'll just draw the window
+        # Draw rectangle over sliding window for debugging and easier visual
         clone = resized.copy()
-        cv2.rectangle(clone, (x, y), (x + winW, y + winH), (255, 0, 180), 20) # ORIGINAL COLOR IS 0,255,0
-        # Add rect to area already saved
-        clone = cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 20)
+        cv2.rectangle(clone, (x, y), (x + winW, y + winH), (255, 0, 180), 20)
+        # TESTING BELOW
+        # Add rect to failing area already saved
+        cv2.rectangle(clone, (BadX1, BadY1), (BadX2, BadY2), (0, 100, 255), 20)
         cloneResize = cv2.resize(clone, (1728, 972))
         cv2.imshow("Window", cloneResize)
         cv2.waitKey(1)
-        # time.sleep(0.5) # sleep time in ms after each window step
+        time.sleep(SLEEP_TIME) # sleep time in ms after each window step
         
         # Scans window for matched image
         # ==================================================================================
         # Scans window and grabs cropped image coordinates relative to window
-        win_x1, win_y1, win_x2, win_y2 = get_match(window, resizedCrop, x, y)
+        win_x1, win_y1, win_x2, win_y2, matchedCL = getMatch(window, resizedCrop, x, y)
         
         # Saves cropped image and names with coordinates
         if win_x1 != "null":
@@ -137,32 +144,55 @@ for (resized, resizedCrop) in pyramid(image, compareCrop, scale=1.5):
             y2 = y + win_y2
             
             # Makes sure same image does not get saved as different names
-            if y1 >= (prev_y1 + round(stepSize/3) ) or y1 <= (prev_y1 - round(stepSize/3) ):
+            if y1 >= (prev_y1 + round(stepSizeY/3) ) or y1 <= (prev_y1 - round(stepSizeY/3)):
                 rowNum += 1
                 colNum = 1
+                sameCol = False
             else:
-                if x1 >= (prev_x1 + round(stepSize/3) ) or x1 <= (prev_x1 - round(stepSize/3) ):
+                if x1 >= (prev_x1 + round(stepSizeX/3) ) or x1 <= (prev_x1 - round(stepSizeX/3)):
                     colNum += 1
+                    prev_matchedCL = 0
+                    sameCol = False
+                else: 
+                    sameCol = True
             
             # NEEDS A CHECK TO SEE IF FIRST X IN PREVIOUS Y-ROW IS THE SAME
             #   IF IT ISN'T, THEN MAKE PREVIOUS FIRST X IN PREVIOUS ROW
             #   HAVE A COLUMN_NUMBER += 1 AND DELETE OLD SAVE AND RESAVE
             #   WITH NEW NAME
             
-            # Gets cropped image and saves cropped image
-            croppedImage = window[win_y1:win_y2, win_x1:win_x2]
-            cv2.imwrite("./Images/Cropped_Images/L{}-Row_{}-Col_{}.jpg".format(layer, rowNum, colNum), croppedImage)
+            if (sameCol == False) or (sameCol == True and matchedCL > prev_matchedCL): 
+                # Gets cropped image and saves cropped image
+                croppedImage = window[win_y1:win_y2, win_x1:win_x2]
+                cv2.imwrite("./Images/Cropped_Images/L{}-Row_{}-Col_{}.jpg".format(layer, rowNum, colNum), croppedImage)
+                # Splits cropped images to folders with potential defects and no defects
+                if matchedCL > SPLIT_MATCHES_CL:
+                    cv2.imwrite("./Images/Splitted_Cropped_Images/No_Defects/L{}-Row_{}-Col_{}-CL_{}.jpg".format(layer, rowNum, colNum, round(matchedCL, 2)), croppedImage)
+                else: 
+                    # TESTING BELOW
+                    BadX1 = x1
+                    BadY1 = y1
+                    BadX2 = x2
+                    BadY2 = y2
+                    cv2.imwrite("./Images/Splitted_Cropped_Images/Potential_Defects/L{}-Row_{}-Col_{}-CL_{}.jpg".format(layer, rowNum, colNum,  round(matchedCL, 2)), croppedImage)
+                # If previous same Row and Column will be saved twice, deletes first one
+                if sameCol == True and matchedCL > prev_matchedCL:
+                    if "L{}-Row_{}-Col_{}-CL_{}.jpg".format(layer, rowNum, colNum, round(prev_matchedCL, 2)) in os.listdir("./Images/Splitted_Cropped_Images/No_Defects/"): 
+                        os.remove("./Images/Splitted_Cropped_Images/No_Defects/L{}-Row_{}-Col_{}-CL_{}.jpg".format(layer, rowNum, colNum, round(prev_matchedCL, 2)))
+                    if "L{}-Row_{}-Col_{}-CL_{}.jpg".format(layer, rowNum, colNum, round(prev_matchedCL, 2)) in os.listdir("./Images/Splitted_Cropped_Images/Potential_Defects/"): 
+                        os.remove("./Images/Splitted_Cropped_Images/Potential_Defects/L{}-Row_{}-Col_{}-CL_{}.jpg".format(layer, rowNum, colNum, round(prev_matchedCL, 2)))
             
             prev_y1 = y1
             prev_x1 = x1
+            prev_matchedCL = matchedCL
             
+            # Draws orange boxes around bad dies
+            if layer == 0:
+                # Separate copy of resized with all bad dies showing orange boxes
+                # TESTING BELOW
+                # Add rect to failing area already saved
+                cv2.rectangle(clone2, (BadX1, BadY1), (BadX2, BadY2), (0, 100, 255), 20)
         # ==================================================================================
     
+    cv2.imwrite("./Images/Image_with_Failing_Dies_Overlay/ImageWithBoxes.jpg", clone2)
     layer += 1
-
-
-# if __name__ == "__main__":
-#     main()
-    
-    
-    
